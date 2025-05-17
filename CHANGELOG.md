06bf88b44ce189406d8b3bb1a526623e59fc990a
Kamil Stachowski <kamil.stachowski@gmail.com>
2025-05-17 13:44:27 +0200

    0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 0.1.1 (17.05.2025)
+- added terminator
+- tweaks to Lualine
+
 
 ## 0.1.0 (12.05.2025)
 
--- lua/lualine/themes/vinyl.lua
+++ lua/lualine/themes/vinyl.lua
@@ -4,37 +4,37 @@ return {
 
 	normal = {
 		a = {fg=p.beige0,bg=p.teal1,bold=true},
-		b = {fg=p.beige0,bg=p.beige80},
-		c = {fg=p.beige0,bg=p.beige60},
+		b = {fg=p.beige0,bg=p.beige60},
+		c = {fg=p.beige0,bg=p.beige40},
 	},
 
 	insert = {
 		a = {fg=p.beige0,bg=p.blue1,bold=true},
-		b = {fg=p.beige0,bg=p.beige80},
-		c = {fg=p.beige0,bg=p.beige60},
+		b = {fg=p.beige0,bg=p.beige60},
+		c = {fg=p.beige0,bg=p.beige40},
 	},
 
 	visual = {
 		a = {fg=p.beige0,bg=p.purple1,bold=true},
-		b = {fg=p.beige0,bg=p.beige80},
-		c = {fg=p.beige0,bg=p.beige60},
+		b = {fg=p.beige0,bg=p.beige60},
+		c = {fg=p.beige0,bg=p.beige40},
 	},
 
 	replace = {
 		a = {fg=p.beige0,bg=p.orange2,bold=true},
-		b = {fg=p.beige0,bg=p.beige80},
-		c = {fg=p.beige0,bg=p.beige60},
+		b = {fg=p.beige0,bg=p.beige60},
+		c = {fg=p.beige0,bg=p.beige40},
 	},
 
 	command = {
 		a = {fg=p.beige0,bg=p.green1,bold=true},
-		b = {fg=p.beige0,bg=p.beige80},
-		c = {fg=p.beige0,bg=p.beige60},
+		b = {fg=p.beige0,bg=p.beige60},
+		c = {fg=p.beige0,bg=p.beige40},
 	},
 
 	inactive = {
-		a = {fg=p.beige0,bg=p.beige40,bold=true},
-		b = {fg=p.beige0,bg=p.beige40},
+		a = {fg=p.beige0,bg=p.beige20,bold=true},
+		b = {fg=p.beige0,bg=p.beige20},
 		c = {fg=p.beige0,bg=p.beige20},
 	},
 
